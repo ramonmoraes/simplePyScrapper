d883c9e92c2dd8b8b08b3fea139d10b64e7244e6
Ramon Moraes <ramon.moraes94@gmail.com>
2018-08-11 18:21:08 -0300

    Create crawlerclass

--- archCrawler.py
+++ archCrawler.py
@@ -1,4 +1,12 @@
+import requests
 from bs4 import BeautifulSoup
 
 CRAWLER_URL = 'https://www.archdaily.com.br/br/899761/todays-rising-stars-in-design-metropolis-magazine-reveals-their-picks'
-print(CRAWLER_URL)
+
+class Crawler():
+    def __init__(self, url):
+        self.html = requests.get(url).text
+        pass
+
+c = Crawler(CRAWLER_URL)
+print(c.html)
