1f8d71388f914c28e3ca15d56f83fe2b98d29e2d
Ramon Moraes <ramon.moraes94@gmail.com>
2018-08-12 19:17:34 -0300

    getting title

--- archCrawler.py
+++ archCrawler.py
@@ -4,9 +4,30 @@ from bs4 import BeautifulSoup
 CRAWLER_URL = 'https://www.archdaily.com.br/br/899761/todays-rising-stars-in-design-metropolis-magazine-reveals-their-picks'
 
 class Crawler():
-    def __init__(self, url):
-        self.html = requests.get(url).text
+    def __init__(self, options):
+        self.html = requests.get(options['url']).text
+        self.parsed = BeautifulSoup(self.html, 'html.parser')
+        self.cssStructure = options['cssStructure']
+        self.get_snippet()
+        # self.find_links()
         pass
 
-c = Crawler(CRAWLER_URL)
-print(c.html)
+    # def find_links(self):
+    #     links = self.parsed.findAll('a')
+    #     print(links)
+    #     pass
+
+    def get_snippet(self):
+        print(self.parsed.find('h1', {'class':'afd-title-big'}).string)
+        pass
+
+
+cssSnipetStructure = {
+    'title': 'h1.afd-title',
+    'text': 'article'
+}
+
+c = Crawler({
+    'url': CRAWLER_URL,
+    'cssStructure': cssSnipetStructure
+})
