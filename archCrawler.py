65539f7caf26a32240a893258b9796489ba58ed6
Ramon Moraes <ramon.moraes94@gmail.com>
2018-08-12 23:08:22 -0300

    add Elements as import on crawler

--- archCrawler.py
+++ archCrawler.py
@@ -1,4 +1,6 @@
 import requests
+
+from Elements import Elements
 from bs4 import BeautifulSoup
 
 CRAWLER_URL = 'https://www.archdaily.com.br/br/899761/todays-rising-stars-in-design-metropolis-magazine-reveals-their-picks'
