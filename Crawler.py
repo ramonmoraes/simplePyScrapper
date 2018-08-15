c1b1609d7444f0afb441c5ea30760fc22b818cb9
Ramon Moraes <ramon.moraes94@gmail.com>
2018-08-14 21:05:55 -0300

    remove unsued stuff from crawler

--- Crawler.py
+++ Crawler.py
@@ -1,13 +1,9 @@
 import requests
 
-from Elements import Elements
 from bs4 import BeautifulSoup
-
-CRAWLER_URL = 'https://www.archdaily.com.br/br/899761/todays-rising-stars-in-design-metropolis-magazine-reveals-their-picks'
 
 class Crawler():
     def __init__(self, url):
-        self.elements = Elements(requests.get(url).text)
         pass
 
     def get_snippet(self):
