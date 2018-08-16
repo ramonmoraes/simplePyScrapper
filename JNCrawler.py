672966460b0cd66bf081d3d870f82b3ce686bce9
Ramon Moraes <ramon.moraes94@gmail.com>
2018-08-15 23:40:17 -0300

    add JNCrawler

---
+++ JNCrawler.py
@@ -0,0 +1,21 @@
+from Crawler import Crawler
+from bs4 import BeautifulSoup
+
+class JNCrawler(Crawler):
+    URL = 'https://jovemnerd.com.br/nerdbunker/vikings-quinta-temporada-ganha-trailer-brutal/'
+
+    def __init__(self):
+        super().__init__(self.URL)
+
+    def get_title(self):
+        return self.elements.find('h1', {'class': 'entry-title'}).get_text()
+    
+    def get_text(self):
+        article = self.elements.find('div', {'class':'entry-content'})
+        return article.findAll("p" , recursive=True)
+
+    def get_img(self):
+        return self.elements.find('div', {'class':'ytp-cued-thumbnail-overlay-image'}).attr('style')
+
+jn = JNCrawler()
+print(jn.get_snippet())
