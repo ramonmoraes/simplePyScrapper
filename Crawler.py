97c3e86990cbcc294ce5e980cfeecd69f1acc706
Ramon Moraes <ramon.moraes94@gmail.com>
2018-08-16 23:05:46 -0300

    getting crawleable links

--- Crawler.py
+++ Crawler.py
@@ -1,16 +1,26 @@
 import requests
+import re
 from bs4 import BeautifulSoup
 
 class Crawler():
     def __init__(self, url):
         self.elements = BeautifulSoup(requests.get(url).text, 'html.parser')
-        self.get_links()
+        self.domain = self.get_domain(url)
+        self.links = self.get_links()
+        print(self.links)
         pass
+
+    def get_domain(self, url):
+        domainRegex = '(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n?]+)' 
+        return re.search(domainRegex, url,re.IGNORECASE).group(1)
 
     def get_links(self):
         everySiteLink = self.elements.findAll('a')
+        link_list = []
         for link in everySiteLink:
-            print(link.get('href'))
+            if self.domain in link.get('href'):
+                link_list.append(link.get('href'))
+        return link_list
 
     def get_snippet(self):
         return {
