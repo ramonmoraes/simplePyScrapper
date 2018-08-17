a00b6ad7b80dad29023b07f63b8211481e7704c2
Ramon Moraes <ramon.moraes94@gmail.com>
2018-08-16 22:44:22 -0300

    create get_links method

--- Crawler.py
+++ Crawler.py
@@ -4,7 +4,13 @@ from bs4 import BeautifulSoup
 class Crawler():
     def __init__(self, url):
         self.elements = BeautifulSoup(requests.get(url).text, 'html.parser')
+        self.get_links()
         pass
+
+    def get_links(self):
+        everySiteLink = self.elements.findAll('a')
+        for link in everySiteLink:
+            print(link.get('href'))
 
     def get_snippet(self):
         return {
