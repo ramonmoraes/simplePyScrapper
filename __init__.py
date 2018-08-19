be4229269843e70bc7e154084e44fb5dff6bb9ea
Ramon Moraes <ramon.moraes94@gmail.com>
2018-08-19 17:16:32 -0300

    writing down json file

--- __init__.py
+++ __init__.py
@@ -1,2 +1,16 @@
-from utils import SnippetWritter
+import json
 
+from utils.SnippetWritter import SnippetWritter
+from crawler.ArchCrawler import ArchCrawler
+
+arc = ArchCrawler()
+links = arc.links
+if (len(links) > 40):
+    links = links[30:]
+
+snippets = {
+    'snippets' : list(filter(lambda link: ArchCrawler(link).get_snippet() != None, links))
+}
+
+with open('./data/data.json', 'w') as jsonFile:
+    json.dump(snippets, jsonFile, ensure_ascii=False)
