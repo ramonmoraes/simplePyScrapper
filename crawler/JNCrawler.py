3132f4c910ca7a7df5124d8432c1cce63eef70a3
Ramon Moraes <ramon.moraes94@gmail.com>
2018-08-18 23:54:27 -0300

    getting right text at JNcrawler

--- crawler/JNCrawler.py
+++ crawler/JNCrawler.py
@@ -14,7 +14,11 @@ class JNCrawler(Crawler):
     
     def get_text(self):
         article = self.elements.find('div', {'class' : 'entry-content'})
-        return article.findAll("p" , recursive=True)
+        articles = article.findAll("p" , recursive=True)
+        text = ''
+        for paragraphs in articles:
+            text += paragraphs.get_text()
+        return text
 
     def get_img(self):
         return self.elements.find('div', {'class' : 'ytp-cued-thumbnail-overlay-image'})
