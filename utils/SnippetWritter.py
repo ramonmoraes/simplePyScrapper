5c2fbe110d79eb9dc74332fe947b9eaa1335d5af
Ramon Moraes <ramon.moraes94@gmail.com>
2018-08-19 00:04:21 -0300

    SnippetWritter v1

--- utils/SnippetWritter.py
+++ utils/SnippetWritter.py
@@ -1,9 +1,12 @@
 import json
 from crawler.JNCrawler import JNCrawler
 
-
-jn = JNCrawler('https://jovemnerd.com.br/nerdbunker/clash-royale-league-latam-adiada/')
-jsonData = json.dumps(jn.get_snippet())
-
-with open('data/data.json', 'w') as jsonFile:
-    json.dump(jsonData, jsonFile, ensure_ascii=False)
+class SnippetWritter():
+    __init__(self, crawler, filePath = 'data/data.json'):
+        self.crawler = crawler
+        self.filePath = filePath
+    
+    def writeFile():
+        jsonData = json.dumps(self.crawler.get_snippet())
+        with open(self.filePath, 'w') as jsonFile:
+            json.dump(jsonData, jsonFile, ensure_ascii=False)
