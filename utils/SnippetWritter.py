609ca89fb9992f195a666240ab4e97ec26e2f8d0
Ramon Moraes <ramon.moraes94@gmail.com>
2018-08-19 00:01:37 -0300

    SnippetWritter mvp

--- utils/SnippetWritter.py
+++ utils/SnippetWritter.py
@@ -1,5 +1,9 @@
+import json
 from crawler.JNCrawler import JNCrawler
 
+
 jn = JNCrawler('https://jovemnerd.com.br/nerdbunker/clash-royale-league-latam-adiada/')
-title = jn.get_snippet()['title']
-print(title)
+jsonData = json.dumps(jn.get_snippet())
+
+with open('data/data.json', 'w') as jsonFile:
+    json.dump(jsonData, jsonFile, ensure_ascii=False)
