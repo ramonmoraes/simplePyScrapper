9a1d554144ca7458383a7894bc81512560d322e8
Ramon Moraes <ramon.moraes94@gmail.com>
2018-08-19 12:39:38 -0300

    fix SnippetWritter class

--- utils/SnippetWritter.py
+++ utils/SnippetWritter.py
@@ -2,11 +2,11 @@ import json
 from crawler.JNCrawler import JNCrawler
 
 class SnippetWritter():
-    __init__(self, crawler, filePath = 'data/data.json'):
+    def __init__(self, crawler, filePath='data/data.json'):
         self.crawler = crawler
         self.filePath = filePath
     
-    def writeFile():
+    def writeFile(self):
         jsonData = json.dumps(self.crawler.get_snippet())
         with open(self.filePath, 'w') as jsonFile:
             json.dump(jsonData, jsonFile, ensure_ascii=False)
