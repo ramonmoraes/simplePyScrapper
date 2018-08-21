b427fdcd9feb8ce9026d44fcca53befe2369fd56
Ramon Moraes <ramon.moraes94@gmail.com>
2018-08-21 08:53:49 -0300

    SnippetWriiter writing locally

--- utils/SnippetWritter.py
+++ utils/SnippetWritter.py
@@ -5,8 +5,23 @@ class SnippetWritter():
     def __init__(self, crawler, filePath='data/data.json'):
         self.crawler = crawler
         self.filePath = filePath
-    
-    def writeFile(self):
-        jsonData = json.dumps(self.crawler.get_snippet())
+        self.write()
+        pass
+
+    def write():
+        snippetList = []
+        links = self.crawler.links
+        if (len(links) > 40):
+            links = links[30:]
+
+        for link in links:
+            print('Crawling page :' + link)
+            snippet = self.crawler(link).get_snippet() 
+            if (snippet != None):
+                snippetList.append(snippet)
+        
+        self.writeFile({ 'snippets': snippetList })
+
+    def writeFile(self, jsonData):
         with open(self.filePath, 'w') as jsonFile:
             json.dump(jsonData, jsonFile, ensure_ascii=False)
