0a843ada104619b3d108e0aca5706f2561617282
Ramon Moraes <ramon.moraes94@gmail.com>
2018-08-22 23:55:56 -0300

    Snippet using decorator

--- snippet/Snippet.py
+++ snippet/Snippet.py
@@ -1,14 +1,16 @@
 from pymongo import MongoClient
 
 from snippet.__init__ import get_db
-
+from snippet.decorator import info_required
+    
 class Snippet():
     def __init__(self, url, title, text, img):
         self.url = url
         self.title = title
         self.text = text
         self.img = img
-    
+
+    @info_required
     def get(self):
         return {
             'url': self.url,
