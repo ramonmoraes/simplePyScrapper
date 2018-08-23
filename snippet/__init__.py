29816dbbec2d82ec8beeb1f66e770102dd22d58e
Ramon Moraes <ramon.moraes94@gmail.com>
2018-08-22 23:29:00 -0300

    add connection_uri back

--- snippet/__init__.py
+++ snippet/__init__.py
@@ -3,5 +3,6 @@ import os
 from pymongo import MongoClient
 
 def get_db():
+    connection_uri = 'mongodb://{}:{}@ds023463.mlab.com:23463/qqrcoisa'
     client = MongoClient(connection_uri.format(os.environ['mongoUser'], os.environ['mongoPassword']))
     db = client['qqrcoisa']
