9c3b9bcaf8ffd89df8d953e18702b3fe671d8bf4
Ramon Moraes <ramon.moraes94@gmail.com>
2018-08-25 13:07:57 -0300

    add save logic to Scrapper

--- scrapper/Scrapper.py
+++ scrapper/Scrapper.py
@@ -1,12 +1,26 @@
 class Scrapper():
-    def __init__(self, parser, save = False):
+    def __init__(self, parser, url, save = False):
         self.parser = parser
-        self.links = parser.links
+        self.url = url
         self.snippet_list = []
+        self.save = save
     
     def get_snippet(self):
-        return self.parser.get_snippet().get_dict()
+        return self.parser.get_snippet()
     
     def scrap(self, deepness = 0):
-        for link in self.links:
+        links = self.parser(self.url).links
+        for link in links:
             print("Scrapping link" + link)
+            newParser = self.parser(link)
+            self.handle_snippet(newParser.get_snippet())
+        
+    def handle_snippet(self, snippet):
+        s_dict = snippet.get_dict()
+        if s_dict == None:
+            return
+        if self.save == True:
+            snippet.save_db()
+        else:
+            print("[Getting snippet]")
+            print(s_dict)
