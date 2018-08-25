eaa838a920fd943524e362b6aa49607adb58b4a1
Ramon Moraes <ramon.moraes94@gmail.com>
2018-08-25 19:46:55 -0300

    add recursive scrap logic

--- scrapper/Scrapper.py
+++ scrapper/Scrapper.py
@@ -1,20 +1,44 @@
 import time
+import re
 
 class Scrapper():
-    def __init__(self, parser, url, save = False, request_throttle = 0):
+    def __init__(self, parser, url, save = False, request_throttle = 1, max_links = 5):
         self.parser = parser
         self.url = url
-        self.snippet_list = []
+        self.parser_list = [parser(url)]
         self.save = save
         self.request_throttle = request_throttle
+        self.max_links = max_links
     
     def scrap(self, deepness = 0):
-        links = self.parser(self.url).links
-        for link in links:
-            print("Scrapping link" + link)
-            newParser = self.parser(link)
+            links = self.parser_list[0].links[:self.max_links]
+            print("[Scrapper] Starting with {} links to be parsed".format(len(links)))
+            self._scrap(deepness, links)
+
+    def _scrap(self, deepness, links):
+        diff_links = []
+        for new_parser in self.parser_list:
+            diff_links = self.get_unique_list(new_parser.links, links)
+        diff_links = diff_links[:self.max_links]
+        print("{} diff links found: ".format(len(links)))
+
+        for link in diff_links:
+            print("[Scrapper] scrapping {}".format(link))
+            self.parser_list.append(self.parser(link))
             time.sleep(self.request_throttle)
-            self.handle_snippet(newParser.get_snippet())
+        print("{} links were parsed".format(len(links)))
+        if deepness > 0:
+            self._scrap(deepness - 1, links + diff_links)
+            
+        self.parse()
+
+    def parse(self):
+        print("[Scrapper] Start parsing")
+        for parser in self.parser_list:
+            self.handle_snippet(parser.get_snippet())
+    
+    def get_unique_list(self, appended_list, compared_list):
+        return list(set(appended_list) - set(compared_list))
 
     def handle_snippet(self, snippet):
         s_dict = snippet.get_dict()
@@ -23,5 +47,4 @@ class Scrapper():
         if self.save == True:
             snippet.save_db()
         else:
-            print("[Getting snippet]")
-            print(s_dict)
+            print("[Got snippet]")
