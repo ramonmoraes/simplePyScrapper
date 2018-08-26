f7f7606de823290a6f68d67b493ade626816e7d3
Ramon Moraes <ramon.moraes94@gmail.com>
2018-08-26 11:21:25 -0300

    add arg logic to __init__.py

--- __init__.py
+++ __init__.py
@@ -1,12 +1,19 @@
-import json
-import os
+from sys import argv
 
 from scrapper.parser.ArchParser import ArchParser
 from scrapper.Scrapper import Scrapper
 
-scrapper = Scrapper(
-    ArchParser,
-    'https://archdaily.com.br/br/899761/todays-rising-stars-in-design-metropolis-magazine-reveals-their-picks'
-    )
-scrapper.scrap(1)
-print(len(scrapper.parser_list))
+
+if argv[1] == 'web':
+    from view.__init__ import app
+    app.run()
+else:
+    print('not web')
+# url = 'https://archdaily.com.br/br/899761/todays-rising-stars-in-design-metropolis-magazine-reveals-their-picks'
+
+# scrapper = Scrapper(
+#     ArchParser,
+#     url,
+#     False # Save
+#     )
+# scrapper.scrap(1)
