7a2783e163e8600638b9da7d24844b12c158ce51
Ramon Moraes <ramon.moraes94@gmail.com>
2018-08-26 23:26:52 -0300

    add scrapperMap

---
+++ scrapper/ScrapperMap.py
@@ -0,0 +1,12 @@
+from scrapper.parser import *
+
+scrapper_map = [
+    {
+        'collection': 'arch',
+        'parser': ArchParser
+    },
+    {
+        'collection': 'jovemnerd',
+        'parser': JNParser
+    }
+]
