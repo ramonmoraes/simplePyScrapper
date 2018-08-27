8d7f263c60e5231948577453cfb6b431208eddcb
Ramon Moraes <ramon.moraes94@gmail.com>
2018-08-26 23:23:32 -0300

    basic view showing based on query passed

--- view/__init__.py
+++ view/__init__.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from snippet.__init__ import get_db
 app = Flask(__name__)
 
 @app.route("/")
 def simple_view():
-    collection = get_db()['snippets']
+    collection_name = 'snippets'
+    query_colletion = user = request.args.get('collection')
+    if query_colletion != None:
+        collection_name = query_colletion
+
+    collection = get_db()[collection_name]
     cursor = collection.find({})
     arr_sni = list(map(lambda x: x, cursor))
     return render_template('index.html', snippets=arr_sni)
