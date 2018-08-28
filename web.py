f6bba49fa0b0eed01c8138e00b86a09dbca0b8a7
Ramon Moraes <ramon.moraes94@gmail.com>
2018-08-27 23:45:50 -0300

    remove unused stuff from web

--- web.py
+++ web.py
@@ -1,7 +1,3 @@
-from sys import argv
-
-from scrapper.parser.ArchParser import ArchParser
-from scrapper.Scrapper import Scrapper
 from server import app
 
 @app.shell_context_processor
@@ -11,13 +7,3 @@ def make_shell_context():
 if __name__ == '__main__':
   app.run()
 
-# else:
-#     print('not web')
-# url = 'https://archdaily.com.br/br/899761/todays-rising-stars-in-design-metropolis-magazine-reveals-their-picks'
-
-# scrapper = Scrapper(
-#     ArchParser,
-#     url,
-#     False # Save
-#     )
-# scrapper.scrap(1)
